df2d71cc0eea8cba76ef006af4b6adeea33bdca4
John Clark <socrates37@gmail.com>
2016-03-03 14:30:55 -0700

    smooth vertical scrolling is a go

--- FamilyHistoryPrimer/CategoryViewController.swift
+++ FamilyHistoryPrimer/CategoryViewController.swift
@@ -10,10 +10,45 @@ import UIKit
 
 class CategoryViewController: UIViewController {
     @IBOutlet weak var navItem: UINavigationItem!
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navItem.title = "Family History: A Primer"
+        
+        categoryCollectionView.dataSource = self
+    }
+    
+    // Set insets appropriately
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Set CategoryCollectionView top and bottom insets
+        var insets = self.categoryCollectionView.contentInset
+        let value = (self.categoryCollectionView.frame.height - (self.categoryCollectionView.collectionViewLayout as! UICollectionViewFlowLayout).itemSize.height) * 0.5
+        insets.top = value
+        insets.bottom = value
+        self.categoryCollectionView.contentInset = insets
+    }
+}
+
+extension CategoryViewController: UICollectionViewDataSource {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("categoryCell", forIndexPath: indexPath)
+
+        cell.backgroundColor = UIColor.blueColor()
+        
+        return cell
+        
     }
 }
