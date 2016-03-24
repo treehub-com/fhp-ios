59dac9734a1a6b3c2b3852f952a78a86ae6c044e
John Clark <socrates37@gmail.com>
2016-03-24 14:00:29 -0600

    lesson card styling

--- FamilyHistoryPrimer/LessonCardView.swift
+++ FamilyHistoryPrimer/LessonCardView.swift
@@ -26,30 +26,25 @@ class LessonCardView: UIView {
         
         self.addSubview(xibView)
 
-        let cardMaskPath = UIBezierPath(roundedRect: xibView.bounds,byRoundingCorners: .AllCorners, cornerRadii: CGSize(width: 10.0, height: 10.0))
-        let cardMaskLayer = CAShapeLayer(layer: cardMaskPath)
-        cardMaskLayer.frame = xibView.bounds
-        cardMaskLayer.path = cardMaskPath.CGPath
-        xibView.layer.mask = cardMaskLayer
-        
-        let contentMaskPath = UIBezierPath(roundedRect: contentView.bounds,byRoundingCorners: .AllCorners, cornerRadii: CGSize(width: 5.0, height: 5.0))
-        let contentMaskLayer = CAShapeLayer(layer: contentMaskPath)
-        contentMaskLayer.frame = contentView.bounds
-        contentMaskLayer.path = contentMaskPath.CGPath
-        contentView.layer.mask = contentMaskLayer
+        contentView.layer.shadowColor = UIColor.blackColor().CGColor
+        contentView.layer.shadowOffset = CGSize(width: 0, height: 7)
+        contentView.layer.shadowOpacity = 0.37
+        contentView.layer.shadowRadius = 3
         
     }
 
     func layout() {
         lessonTitleLabel.text = lesson.title
+        lessonTitleLabel.textColor = lesson.color
         lessonSubtitleLabel.text = lesson.subtitle
+        lessonSubtitleLabel.textColor = lesson.color
         lessonImageView.image = lesson.img
-        xibView.backgroundColor = lesson.color
         if (lesson.sections.count == 1) {
             sectionCountLabel.text = String(lesson.sections.count) + " section"
         } else {
             sectionCountLabel.text = String(lesson.sections.count) + " sections"
         }
+        sectionCountLabel.textColor = lesson.color
     }
     
 }
